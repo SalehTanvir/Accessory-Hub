4eb9e7350a11fa9b505eef0ee56715ca7cad7d79
SalehTanvir <salehtanvir852@gmail.com>
2026-03-08 23:21:23 +0600

    created user model, auth controller, routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");   // import database connection
+const connectDB = require("./config/db");// import database connection
+const authRoutes = require("./routes/authRoutes");   
 
 const app = express();
 
@@ -9,6 +10,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
