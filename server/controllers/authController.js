42f16b00b0ec3e9583e3d88d2156b7da0e8a2f75
SalehTanvir <salehtanvir852@gmail.com>
2026-03-10 23:56:18 +0600

    created login api, updated auth routes, created auth middleware

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,23 +1,33 @@
-const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-exports.registerUser = async (req, res) => {
+exports.loginUser = async (req, res) => {
   try {
 
-    const { name, email, password } = req.body;
+    const { email, password } = req.body;
 
-    // hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.findOne({ email });
 
-    // create user
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword
-    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-    res.status(201).json({
-      message: "User registered successfully",
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      "secretkey",
+      { expiresIn: "7d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
       user
     });
 
---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+
+  let token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+
+  try {
+
+    token = token.split(" ")[1];
+
+    const decoded = jwt.verify(token, "secretkey");
+
+    req.user = decoded;
+
+    next();
+
+  } catch (error) {
+    res.status(401).json({ message: "Token failed" });
+  }
+};
+
+module.exports = protect;
